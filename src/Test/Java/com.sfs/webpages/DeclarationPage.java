c4d3c35ffbaa2e5f788a2433c77b741550a7bd4e
farnsword <h.farnsword@gmail.com>
2016-09-22 14:24:27 +0300

    updated Declaration Page

--- src/Test/Java/com.sfs/webpages/DeclarationPage.java
+++ src/Test/Java/com.sfs/webpages/DeclarationPage.java
@@ -29,32 +29,81 @@ public class DeclarationPage {
     @FindBy(css = "div#HZ")
     private WebElement declarType;
 
+    @FindBy(id = HZY)
+	private WebElement preDeclarYear;
     @FindBy(xpath = ".//input[@id = \"0_HZY\"]")
     private WebElement declarYear;
-
+   	
+ 	@FindBy(id = HNAME)
+	private WebElement prePayerName;
     @FindBy(xpath = ".//input[@id=\"0_HNAME\"]")
     private WebElement payerName;
-
+   	
+ 	@FindBy(id = HTIN)
+	private WebElement prePegisrOrPassport;
     @FindBy(xpath = ".//input[@id = \"0_HTIN\"]")
     private WebElement registrOrPassport;
-
+   	
+ 	@FindBy(id = HREG)
+	private WebElement prePayerObl;
     @FindBy (xpath = ".//input[@id=\"0_HREG\"]")
     private WebElement payerObl;
-
-    @FindBy (css = "#HRAJ")
+   	
+ 	@FindBy(id = HRAJ)
+    private WebElement prePayerRegion;
+    @FindBy (xpath = ".//input[@id=\”0_HRAJ\"]”)
     private WebElement payerRegion;
-
-    @FindBy (css = "#HCITY")
+   	
+ 	@FindBy(id = HCITY)
+	private WebElement prePayerCity;
+    @FindBy (xpath = ".//input[@id=\”0_HCITY\"]”)
     private WebElement payerCity;
-
+   	
+ 	@FindBy(id = HSTREET)
+	private WebElement prePayerStreet;
     @FindBy (xpath = ".//input[@id=\"0_HSTREET\"]")
     private WebElement payerStreet;
-
+   	
+ 	@FindBy(id = HBUILD)
+	private WebElement prePayerBuild;
     @FindBy (xpath = ".//input[@id=\"0_HBUILD\"]")
     private WebElement payerBuild;
-
+   	
+    @FindBy(id = HCORP)
+    private WebElement prePayerCorp;
     @FindBy (xpath = ".//input[@id=\"0_HCORP\"]")
     private WebElement payerCorp;
+   	
+    @FindBy(id = HAPT)
+    private WebElement prePayerApt;
+    @FindBy (xpath = “.//input[@id=\”0_HAPT\”]”
+    private WebElement payerApt;
+
+	@FindBy(id = HZIP)
+	private WebElement prePayerZip
+	@FindBy(xpath = “.//input[@id=\”0_HZIP\”]”)
+	private WebElement payerZip;
+
+	@FindBy(id = HTEL)
+	private WebElement prePayerTel;
+	@FindBy(xpath = “.//input[@id=\”0_HTEL\”]”)
+	private WebElement payerTel;
+
+	@FindBy(id = HEMAIL)
+	private WebElement prePayerEmail;
+	@FindBy(xpath = “.//input[@id=\”0_HEMAIL\”]”)
+	private WebElement payerEmail;
+            	
+    @FindBy(id = HSTI)
+    private WebElement preAuthority;
+    @FindBy(xpath = “.//input[@id=\”0_HSTI\”]”)
+    private WebElement authority;
+
+    @FindBy(id = H01)
+    private WebElement residentStatus;
+
+    @FindBy(id = H03)
+    private WebElement whoFilled;
 
 
 
@@ -62,8 +111,17 @@ public class DeclarationPage {
     public void setDeclareType(){
 
     }
+    
+    public void clickPreElement(WebElement preElem){
+        preElem.click();
+    }
+    
+    public void setValue(WebElement elem, String value){
+        elem.clear();
+        elem.sendKeys(value);
+    }
 
-
+/*
     public void setDeclarYear(String year) {
         declarYear.clear();
         declarYear.sendKeys(year);
@@ -108,4 +166,5 @@ public class DeclarationPage {
         payerCorp.clear();
         payerCorp.sendKeys(corp);
     }
+*/
 }
